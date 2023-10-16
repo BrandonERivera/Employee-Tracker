ed854a21fdd58da91d68cc78b5db5d82610b7a53
BrandonERivera <brandonellis.rivera@gmail.com>
2023-10-16 01:45:08 -0400

    added references to server and filled in scheme and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ("department 1"),
+        ("department 2"),
+        ("department 3"),
+
+INSERT INTO role(department_id, title, salary)
+VALUES  (1,"role 1", 80000),
+        (2,"role 2", 60000),
+        (2,"role 3", 60000),
+        (3,"role 4", 50000);
+
+INSERT INTO employee(role_id, first_name, last_name, manager_id)
+VALUES  (1,"name1","name2", NULL),
+        (2,"secondname1","secondname2", 1),
+        (3,"thirdname1","thirdname2", 1),
+        (3,"fourthname1", "fourthname2", 3);
+
--- server.js
+++ server.js
@@ -6,22 +6,117 @@ const { clog } = require('./middleware/clog');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(clog);
 
-// const db = mysql.createConnection(
-//     {
-//       host: "localhost",
-//       // MySQL username,
-//       user: "root",
-//       // TODO: Add MySQL password here
-//       password: "",
-//       database: "",
-//     },
-//     console.log(`Connected to  -----db`)
-//   );
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      // MySQL username,
+      user: "root",
+      // TODO: Add MySQL password here
+      password: "B0Rivera6966",
+      database: "company_db",
+    },
+    console.log(`Connected to  company_db`)
+  );
+  
+  app.use((req, res) => {
+    res.status(404).end();
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  questions;
 });
+const questions = [
+inquirer
+  .prompt([
+    {
+
+    },
+
+])];
+//REFERENCES
+
+// db.query("SELECT * FROM course_names", function (err, results) {
+//   console.log(results);
+// });
+//POST
+// app.post("/api/new-movie", ({ body }, res) => {
+//   const sql = `INSERT INTO movies (movie_name)
+//     VALUES (?)`;
+//   const params = [body.movie_name];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: "success",
+//       data: body,
+//     });
+//   });
+// });
+
+//GET
+// app.get("/api/movies", (req, res) => {
+//   const sql = `SELECT id, movie_name AS title FROM movies`;
+
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: "success",
+//       data: rows,
+//     });
+//   });
+// });
+
+//DELETE
+// app.delete("/api/movie/:id", (req, res) => {
+//   const sql = `DELETE FROM movies WHERE id = ?`;
+//   const params = [req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.statusMessage(400).json({ error: res.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: "Movie not found",
+//       });
+//     } else {
+//       res.json({
+//         message: "deleted",
+//         changes: result.affectedRows,
+//         id: req.params.id,
+//       });
+//     }
+//   });
+// });
+
+//UPDATE
+// app.put("/api/review/:id", (req, res) => {
+//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+//   const params = [req.body.review, req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: "Movie not found",
+//       });
+//     } else {
+//       res.json({
+//         message: "success",
+//         data: req.body,
+//         changes: result.affectedRows,
+//       });
+//     }
+//   });
+// });
