f1fe127d1564ea4ebf40013a80226b2086a97cb5
BrandonERivera <brandonellis.rivera@gmail.com>
2023-10-17 21:20:46 -0400

    finished the query prompts, next is readME

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const inquirer = require('inquirer');
+const path = require('path');
 const mysql = require("mysql2");
 const { clog } = require('./middleware/clog');
 
@@ -13,9 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection(
     {
       host: "localhost",
-      // MySQL username,
       user: "root",
-      // TODO: Add MySQL password here
       password: "B0Rivera6966",
       database: "company_db",
     },
@@ -23,14 +22,14 @@ const db = mysql.createConnection(
   );
 
   
-function questions()
+const questions = () =>
 {
   inquirer
     .prompt([
       {
         type: 'list',
         name: 'action',
-        message: "choose a following option.",
+        message: "choose a following action.",
         choices: ["View all departments.","View all roles.","View all employees.", "Add a department.","Add a role.","Add an employee.","Update an employee role."]
       },
     ])
@@ -51,31 +50,118 @@ function questions()
           });
           break;
         case  "View all employees.":
-          db.query("SELECT employee.id AS Employee_id, CONCAT(employee.first_name, employee.last_name) AS Employee_NAME, role.title AS Role, role.salary AS Salary, department.name AS Department, CONCAT(employee.manager_id) AS Manager_Id FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id", function (err, results) {
+          db.query("SELECT employee.id AS Employee_id, CONCAT(employee.first_name, employee.last_name) AS Employee_NAME, role.title AS Role, role.salary AS Salary, department.name AS Department, CONCAT(employee.manager_id) AS Manager_Id FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;", function (err, results) {
             console.log(results);
             questions();
           });
           break;
         case "Add a department.":
-          console.log(action);
-          // start new inquierer, that creates an object for a post request for department
+          inquirer
+            .prompt([
+              {
+                name: "name",
+               type: "input",
+                message: "What is the department Name?",
+              }
+            ])
+            .then(answers => {
+                let sql = `INSERT INTO department (name) VALUES (?)`;
+                let params = [answers.name];
+                db.query(sql, params, (err, result) => {
+                console.log(result);
+                questions();
+              });
+            });
           break;
         case "Add a role.":
-          console.log(action);
-          //starters new inquierer, that creates an object for a post request for role
+          inquirer
+            .prompt([
+              {
+                name: "department_id",
+                type: "input",
+                message: "What is the department id this role belongs to?",
+              },
+              {
+                name: "rolename",
+                type: "input",
+                message: "What is the roles name?",
+              },
+              {
+                name: "rolesalary",
+                type: "input",
+                message: "What is the roles salary?",
+              }
+            ])
+            .then(answers => {
+                let sql = `INSERT INTO role(department_id, title, salary) VALUES (?,?,?)`;
+                let params = [answers.department_id, answers.rolename, answers.rolesalary];
+                console.log(params);
+                db.query(sql, params, (err, result) => {
+                console.log(result);
+                questions();
+              });
+            });
           break;
         case "Add an employee.":
-          console.log(action);
-          //starts new inquirer, that creates an object for a post request for employee
+          inquirer
+          .prompt([
+            {
+              name: "firstname",
+              type: "input",
+              message: "What is the employee first name?",
+            },
+            {
+              name: "lastname",
+              type: "input",
+              message: "What is the employee last name?",
+            },
+            {
+              name: "role_id",
+              type: "input",
+              message: "What is the role id?",
+            },
+            {
+              name: "manager_id",
+              type: "input",
+              message: "What is the manager id #?",
+            }
+          ])
+          .then(answers => {
+              const sql = `INSERT INTO employee(role_id, first_name, last_name, manager_id) VALUES (?,?,?,?)`;
+              const params = [answers.firstname, answers.lastname, answers.role_id, answers.manager_id]
+              console.log(params)
+              db.query(sql, params, (err, result) => {
+              console.log(result);
+              questions();
+            });
+          });
           break;
         case "Update an employee role.":
-          console.log(action);
-          //starts a new inquirer, that creats an object for a put request for employee
-      }
+          inquirer
+            .prompt([
+            {
+              name: "id",
+              type: "input",
+              message: "What is the employees id?",
+            },
+            {
+              name: "role_id",
+              type: "input",
+              message: "What is the role id?",
+            },
+          ])
+          .then(answers =>{
+          let sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+          let params = [answers.role_id, answers.id];
+          db.query(sql, params,(err, result) => {
+            console.log(result)
+            questions();
+          });
+        });
+      };
     })
 };
-//role.salary AS Salary
-//JOIN role ON role.title = role.id Join role r2 ON role.salary = r2.id JOIN department ON department.name = department.id
+
 app.use((req, res) => {
   res.status(404).end();
 });
@@ -85,43 +171,6 @@ app.listen(PORT, () => {
   questions()
 });
 //REFERENCES
-
-// db.query("SELECT * FROM course_names", function (err, results) {
-//   console.log(results);
-// });
-//POST
-// app.post("/api/new-movie", ({ body }, res) => {
-//   const sql = `INSERT INTO movies (movie_name)
-//     VALUES (?)`;
-//   const params = [body.movie_name];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: body,
-//     });
-//   });
-// });
-
-//GET
-// app.get("/api/movies", (req, res) => {
-//   const sql = `SELECT id, movie_name AS title FROM movies`;
-
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: rows,
-//     });
-//   });
-// });
 
 //DELETE
 // app.delete("/api/movie/:id", (req, res) => {
